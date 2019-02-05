12412888e4ada0a8db6a8b2cc77321bd63f6556b
Bolton <bolton.otieno@gmail.com>
2019-02-05 21:49:30 +0300

    create todo app

--- src/App.js
+++ src/App.js
@@ -1,51 +1,79 @@
 import React, { Component } from "react";
-import "./App.css";
-import Header from "./components/layout/Header"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
 import Todos from "./components/todos/Todos";
+import AddTodo from "./components/todos/AddTodo";
+import About from "./components/pages/About";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
   state = {
-    todos: [
-      {
-        id: 1,
-        title: "Workout in the morning",
-        completed: false
-      },
-      {
-        id: 2,
-        title: "Take breakfast",
-        completed: false
-      },
-      {
-        id: 3,
-        title: "Go to work",
-        completed: false
-      }
-    ]
+    todos: []
   };
 
-  // Toggle Complete
-  markComplete = (id) => {
-    this.setState( {todos: this.state.todos.map(todo => {
-      if (todo.id === id){
-        todo.completed = !todo.completed
-      }
-      return todo;
-    }) });
-}
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(res => this.setState({ todos: res.data }));
+  }
 
-// Delete Todo
-delTodo= (id) => {
-  this.setState( {todos: [...this.state.todos.filter(todo => todo.id !== id)]});
-}
+  // Toggle Complete
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  // Delete Todo
+  delTodo = id => {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res =>
+        this.setState({
+          todos: [...this.state.todos.filter(todo => todo.id !== id)]
+        })
+      );
+  };
+
+  // Add Todo
+  addTodo = title => {
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", {
+        title,
+        completed: false
+      })
+      .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
+  };
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Todos todos={this.state.todos} markComplete={this.markComplete}
-          delTodo={this.delTodo}
-        />
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,18 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 function Header() {
-    return (
-        <header style={headerStyle}>
-            <h1>ToDoList</h1>
-        </header>
-    )
+  return (
+    <header style={headerStyle}>
+      <h1>ToDoList</h1>
+      <Link style={linkStyle} to="/">
+        Home
+      </Link>{" "}
+      |{" "}
+      <Link style={linkStyle} to="/about">
+        About
+      </Link>
+    </header>
+  );
 }
 
 const headerStyle = {
-    background: '#333',
+  background: "#333",
+  color: "#fff",
+  textAlign: "center",
+  padding: "10px"
+};
+
+const linkStyle = {
     color: '#fff',
-    textAlign: 'center',
-    padding: '10px'
+    textDecorations: 'none'
 }
 
 export default Header;
