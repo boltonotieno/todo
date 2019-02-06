39400db18025d583339b55929d674bebdcdaebae
Bolton <bolton.otieno@gmail.com>
2019-02-06 07:04:22 +0300

    Updates

---
+++ precache-manifest.2a152e038093d7da7bda3d748f050313.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15c8f989903d6cb3e44e",
+    "url": "/todo/static/css/main.341f6dca.chunk.css"
+  },
+  {
+    "revision": "15c8f989903d6cb3e44e",
+    "url": "/todo/static/js/main.15c8f989.chunk.js"
+  },
+  {
+    "revision": "5ef3b90050b60f0bfacb",
+    "url": "/todo/static/js/1.5ef3b900.chunk.js"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "2539da93a8d1429d1f97e9126e4e997a",
+    "url": "/todo/index.html"
+  }
+];
